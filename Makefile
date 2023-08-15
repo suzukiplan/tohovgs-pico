1a7f3f5bdd6e920e0d9995bb46a5af1677fb4578
suzukiplan <suzukiplan.ys@gmail.com>
2023-08-15 20:15:31 +0900

    Keyboard UI

--- Makefile
+++ Makefile
@@ -1,8 +1,26 @@
-all:
+ROMS = src/rom_small_font.c
+
+all: roms
 	pio run -t upload
 
-build:
+build: roms
 	pio run
+
+roms: tools/bmp2img/bmp2img tools/varext/varext
+	make src/rom_small_font.c
+	make include/roms.hpp
+
+tools/bmp2img/bmp2img: tools/bmp2img/bmp2img.cpp
+	cd tools/bmp2img && make
+
+tools/varext/varext: tools/varext/varext.cpp
+	cd tools/varext && make
+
+src/rom_small_font.c: rom/small_font.bmp
+	tools/bmp2img/bmp2img -t 4x8 rom/small_font.bmp >src/rom_small_font.c
+
+include/roms.hpp: ${ROMS}
+	tools/varext/varext ${ROMS} >include/roms.hpp
 
 format: 
 	make execute-format FILENAME=./src/app.cpp
--- src/app.cpp
+++ src/app.cpp
@@ -3,10 +3,77 @@
  * License under GPLv3: https://github.com/suzukiplan/tohovgs-pico/blob/master/LICENSE.txt
  */
 
+#include "roms.hpp"
 #include <SPI.h>
 #include <TFT_eSPI.h>
 
+#define BGCOLOR 0x1148
+
 static TFT_eSPI gfx(240, 320);
+
+static void printSmallFont(int x, int y, const char* format, ...)
+{
+    char buf[64];
+    va_list args;
+    va_start(args, format);
+    vsnprintf(buf, sizeof(buf), format, args);
+    va_end(args);
+    for (int i = 0; buf[i] && i < 64; i++, x += 4) {
+        if ('0' <= buf[i] && buf[i] <= '9') {
+            gfx.pushImage(x, y, 4, 8, &rom_small_font[(buf[i] - '0') * 32]);
+        } else if ('A' <= buf[i] && buf[i] <= 'Z') {
+            gfx.pushImage(x, y, 4, 8, &rom_small_font[320 + (buf[i] - 'A') * 32]);
+        } else if (' ' == buf[i]) {
+            gfx.fillRect(x, y, 4, 8, BGCOLOR);
+        } else if ('.' == buf[i]) {
+            gfx.pushImage(x, y, 4, 8, &rom_small_font[320 + 832]);
+        } else if (':' == buf[i]) {
+            gfx.pushImage(x, y, 4, 8, &rom_small_font[320 + 832 + 32]);
+        }
+    }
+}
+
+class KeyboardRenderer
+{
+  private:
+    struct Position {
+        int x;
+        int y;
+    } pos;
+    int key;
+
+    void render(int ch)
+    {
+        gfx.fillRect(this->pos.x, this->pos.y, 200, 10, BGCOLOR);
+        // チャネル名と楽器を描画
+        printSmallFont(this->pos.x, this->pos.y, "CH%d TRI ", ch);
+        // 白鍵を描画
+        for (int i = 0; i < 50; i++) {
+            gfx.fillRect(this->pos.x + 32 + i * 4, this->pos.y, 3, 9, 0xFFFF);
+        }
+        // 黒鍵を描画
+        for (int i = 0; i < 49; i++) {
+            switch (i % 7) {
+                case 0:
+                case 2:
+                case 3:
+                case 5:
+                case 6:
+                    gfx.fillRect(this->pos.x + 32 + i * 4 + 2, this->pos.y, 3, 7, 0x0000);
+                    break;
+            }
+        }
+    }
+
+  public:
+    KeyboardRenderer(int ch, int x, int y)
+    {
+        this->pos.x = x;
+        this->pos.y = y;
+        this->key = -1;
+        render(ch);
+    }
+};
 
 void setup()
 {
@@ -21,12 +88,19 @@ void setup()
     gfx.init();
     gfx.startWrite();
     gfx.setRotation(2);
-    gfx.fillScreen(0x003F);
-    gfx.setCursor(0, 0, 2);
-    gfx.setTextColor(0xFFFF);
-    gfx.setTextSize(2);
-    gfx.println("Hello, World!");
-    gfx.println("Using TFT_eSPI");
+    gfx.fillScreen(BGCOLOR);
+
+    printSmallFont(4, 4, "SONG NOT SELECTED");
+    printSmallFont(4, 16, "INDEX     00000  PLAYING 0 OF 0");
+    printSmallFont(4, 24, "LEFT TIME 00:00");
+
+    gfx.drawLine(0, 34, 240, 34, 0x8410);
+    gfx.drawLine(0, 36, 240, 36, 0xFFFF);
+    for (int i = 0; i < 6; i++) {
+        KeyboardRenderer key(i, 4, 40 + i * 10);
+    }
+    gfx.drawLine(0, 101, 240, 101, 0xFFFF);
+    gfx.drawLine(0, 103, 240, 103, 0x8410);
     gfx.endWrite();
 }
 
